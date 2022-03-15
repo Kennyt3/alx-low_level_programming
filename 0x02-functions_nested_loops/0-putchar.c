c45f77f0d84850bc58a6fd8e941f2e45065850d1
Taiwo Mercy <kennyt341@gmail.com>
2022-03-15 08:10:07 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints a word
+ *
+ * Description: Just prints a statement
+ * Return: Always (0) success
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
