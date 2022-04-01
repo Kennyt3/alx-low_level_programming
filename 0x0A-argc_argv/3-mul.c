2aa1f24a2a6654ae7e094226ea5df26384ba3551
Taiwo Mercy <kennyt341@gmail.com>
2022-04-01 05:29:52 -0700

    argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,27 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
- * https://alx-intranet.hbtn.io/user_containers/50400/webterm
- * @argc: The arguments' counter
- * @argv: The arguments' values
- * Return: Always 0 (Success)
+ * main - multiplies two numbers.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int num1, num2, result;
-
 	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		result = num1 * num2;
-		printf("%d\n", result);
-		return (0);
-	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
 }
